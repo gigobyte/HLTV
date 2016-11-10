24de114886272f50dea2bdc1e40747a79bc13f47
gigobyte <gigobest2@gmail.com>
2016-11-10 23:06:09 +0200

    Module init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 //It all begins here
+import cheerio from 'cheerio'
+import fetch from 'isomorphic-fetch'
+
+
+async function getHtlv(): Promise {
+    const response = await fetch('http://www.hltv.org/').then(res => res)
+
+    return response
+}
