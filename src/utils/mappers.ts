c602cfcc524a25ddaf0e68052e059026650cb7ce
twanh <huiskenstwan@gmail.com>
2018-08-25 13:02:12 +0200

    Reverted the code styling to the original styling

--- src/utils/mappers.ts
+++ src/utils/mappers.ts
@@ -1,123 +1,98 @@
-import * as cheerio from "cheerio";
-import * as request from "request";
-import Team from "../models/Team";
-import Veto from "../models/Veto";
-import Player from "../models/Player";
-import { MapStatistic } from "../models/FullTeam";
-import { Outcome, WeakRoundOutcome } from "../models/RoundOutcome";
-import MapSlug from "../enums/MapSlug";
-import * as E from "../utils/parsing";
+import * as cheerio from 'cheerio'
+import * as request from 'request'
+import Team from '../models/Team'
+import Veto from '../models/Veto'
+import Player from '../models/Player'
+import { MapStatistic } from '../models/FullTeam'
+import { Outcome, WeakRoundOutcome } from '../models/RoundOutcome'
+import MapSlug from '../enums/MapSlug'
+import * as E from '../utils/parsing'
 
 export const fetchPage = async (url: string) => {
-  return cheerio.load((await new Promise(resolve => {
-    request.get(url, (_, __, body) => resolve(body));
-  })) as any);
-};
+    return cheerio.load(await new Promise((resolve) => {
+        request.get(url, (_, __, body) => resolve(body))
+    }) as any);
+}
 
-export const toArray = (elements: Cheerio): Cheerio[] =>
-  elements.toArray().map(cheerio);
-export const getMapSlug = (map: string): MapSlug => MapSlug[map];
+export const toArray = (elements: Cheerio): Cheerio[] => elements.toArray().map(cheerio)
+export const getMapSlug = (map: string): MapSlug => MapSlug[map]
 
-export const mapVetoElementToModel = (
-  el: Cheerio,
-  team1: Team,
-  team2: Team
-): Veto => {
-  const [teamName, map] = el
-    .text()
-    .replace(/^\d. /, "")
-    .split(/removed|picked/);
+export const mapVetoElementToModel = (el: Cheerio, team1: Team, team2: Team): Veto => {
+    const [ teamName, map ] = el.text().replace(/^\d. /, '').split(/removed|picked/)
 
-  if (!map || !teamName) {
+    if (!map || !teamName) {
+        return {
+            map: getMapSlug(el.text().split(' ')[1]),
+            type: 'other'
+        }
+    }
+
     return {
-      map: getMapSlug(el.text().split(" ")[1]),
-      type: "other"
-    };
-  }
-
-  return {
-    team: [team1, team2].find(t => t.name === teamName.trim()) as Team,
-    map: getMapSlug(map.trim()),
-    type: el.text().includes("picked") ? "picked" : "removed"
-  };
-};
+        team: [team1, team2].find(t => t.name === teamName.trim()) as Team,
+        map: getMapSlug(map.trim()),
+        type: el.text().includes('picked') ? 'picked' : 'removed'
+    }
+}
 
 export const getMatchPlayer = (playerEl: Cheerio): Player => {
-  const link = playerEl.parent().attr("href");
+    const link = playerEl.parent().attr('href')
 
-  return {
-    name: playerEl.find(".text-ellipsis").text(),
-    id: link ? Number(link.split("/")[2]) : undefined
-  };
-};
+    return {
+        name: playerEl.find('.text-ellipsis').text(),
+        id: link ? Number(link.split('/')[2]) : undefined
+    }
+}
 
-export const getMatchFormatAndMap = (
-  mapText: string
-): { map?: MapSlug; format?: string } => {
-  if (mapText && !mapText.includes("bo")) {
-    return { map: mapText as MapSlug, format: "bo1" };
-  }
+export const getMatchFormatAndMap = (mapText: string): {map?: MapSlug, format?: string} => {
+    if (mapText && !mapText.includes('bo')) {
+        return { map: mapText as MapSlug, format: 'bo1'}
+    }
 
-  if (!mapText) {
-    return {};
-  }
+    if (!mapText) {
+        return {}
+    }
 
-  return { format: mapText };
-};
+    return { format: mapText }
+}
 
-export const mapRoundElementToModel = (team1Id: number, team2Id: number) => (
-  el: Cheerio,
-  i: number
-): WeakRoundOutcome => {
-  const outcomeString = (E.popSlashSource(el) as string).split(".")[0];
-  const outcome = Object.entries(Outcome).find(
-    ([_, v]) => v === outcomeString
-  ) as Outcome | undefined;
+export const mapRoundElementToModel = (team1Id: number, team2Id: number) => (el: Cheerio, i: number): WeakRoundOutcome => {
+    const outcomeString = (E.popSlashSource(el) as string).split('.')[0]
+    const outcome = Object.entries(Outcome).find(([_, v]) => v === outcomeString) as Outcome | undefined
 
-  return {
-    outcome: outcome && (outcome[1] as Outcome),
-    score: el.attr("title"),
-    ctTeam: i < 15 ? team1Id : team2Id,
-    tTeam: i < 15 ? team2Id : team1Id
-  };
-};
+    return {
+        outcome: outcome && outcome[1] as Outcome,
+        score: el.attr('title'),
+        ctTeam: i < 15 ? team1Id : team2Id,
+        tTeam: i < 15 ? team2Id : team1Id
+    }
+}
 
-export const getMapsStatistics = (
-  source: string
-): { [key: string]: MapStatistic } | undefined => {
-  const valueRegex = /"value":"(\d|\.)+%?"/g;
-  const labelRegex = /label":"\w+/g;
-  const splitRegex = /%?"/;
+export const getMapsStatistics = (source: string): {[key: string]: MapStatistic} | undefined => {
+    const valueRegex = /"value":"(\d|\.)+%?"/g
+    const labelRegex = /label":"\w+/g
+    const splitRegex = /%?"/
 
-  if (!source.match(labelRegex)) {
-    return;
-  }
+    if (!source.match(labelRegex)) {
+        return;
+    }
 
-  const maps = (source.match(labelRegex) as RegExpMatchArray).map(
-    x => x.split(':"')[1]
-  );
-  const values = (source.match(valueRegex) as RegExpMatchArray).map(x =>
-    Number(x.split(':"')[1].split(splitRegex)[0])
-  );
+    const maps = (source.match(labelRegex) as RegExpMatchArray).map(x => x.split(':"')[1])
+    const values = (source.match(valueRegex) as RegExpMatchArray).map(x => Number(x.split(':"')[1].split(splitRegex)[0]))
 
-  const getStats = index => ({
-    winningPercentage: values[index],
-    ctWinningPercentage: values[index + maps.length * 1],
-    tWinningPercentage: values[index + maps.length * 2],
-    timesPlayed: values[index + maps.length * 3]
-  });
+    const getStats = index => ({
+        winningPercentage: values[index],
+        ctWinningPercentage: values[index + maps.length * 1],
+        tWinningPercentage: values[index + maps.length * 2],
+        timesPlayed: values[index + maps.length * 3]
+    })
 
-  return maps.reduce(
-    (stats, map, i) => ({
-      ...stats,
-      [MapSlug[map]]: getStats(i)
-    }),
-    {}
-  );
-};
+    return maps.reduce((stats, map, i) => ({
+        ...stats, [MapSlug[map]]: getStats(i)
+    }), {})
+}
 
 export const getTimestamp = (source: string): number => {
-  const [day, month, year] = source.split("/");
+    const [ day, month, year ] = source.split('/')
 
-  return new Date([month, day, year].join("/")).getTime();
-};
+    return new Date([month, day, year].join('/')).getTime()
+}
