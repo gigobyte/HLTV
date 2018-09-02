231fbf6f7a1eff637586fe93615954280ba69542
gigobyte <gigobest2@gmail.com>
2018-09-02 22:10:50 +0300

    Add regression tests

---
+++ __tests__/getMatch.test.ts
@@ -0,0 +1,8 @@
+import HLTV from '../src/'
+
+test('getMatch', async () => {
+    expect(await HLTV.getMatch({id: 2325765})).toMatchSnapshot()
+    expect(await HLTV.getMatch({id: 2302345})).toMatchSnapshot()
+    expect(await HLTV.getMatch({id: 2290099})).toMatchSnapshot()
+    expect(await HLTV.getMatch({id: 2186795})).toMatchSnapshot()
+})
