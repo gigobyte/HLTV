855e1d1f7e63d5d3092841f882d2ff54787972fb
Alan Melo Clappis <alan@eurekalabs.com.br>
2018-09-27 23:22:01 -0300

    Add 'statsId' to getMatch.ts

--- src/models/MapResult.ts
+++ src/models/MapResult.ts
@@ -2,7 +2,8 @@ import MapSlug from '../enums/MapSlug'
 
 interface MapResult {
     readonly name: MapSlug,
-    readonly result: string
+    readonly result: string,
+    readonly statsId?: number
 }
 
 export default MapResult
