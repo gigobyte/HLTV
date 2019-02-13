40d889ed8f0ccb47cd4bc7197db2ccab07380187
Markus Stefanko <markus.stefanko@gmail.com>
2019-02-13 17:30:27 +0700

    npm run pretty

--- src/endpoints/getEvent.ts
+++ src/endpoints/getEvent.ts
@@ -50,13 +50,13 @@ export const getEvent = (config: HLTVConfig) => async ({
   }))
 
   function findEventByName(eventName, relatedEvents) {
-    if(eventName == null || relatedEvents.length == 0) return eventName;
-    for(var event of relatedEvents) {
-      if(event.name == eventName) {
-        return event;
+    if (eventName == null || relatedEvents.length == 0) return eventName
+    for (var event of relatedEvents) {
+      if (event.name == eventName) {
+        return event
       }
     }
-    return eventName;
+    return eventName
   }
 
   const prizeDistribution = toArray($('.placement')).map(placementEl => ({
@@ -66,12 +66,14 @@ export const getEvent = (config: HLTVConfig) => async ({
         .find('.prizeMoney')
         .first()
         .text() || undefined,
-    qualifiesFor:
-      findEventByName(placementEl
+    qualifiesFor: findEventByName(
+      placementEl
         .find('.prizeMoney')
         .first()
         .next()
-        .text() || undefined, relatedEvents),
+        .text() || undefined,
+      relatedEvents
+    ),
     team:
       placementEl.find('.team').children().length !== 0
         ? {
