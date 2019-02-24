05fbc026f3bd04fa126ed14ff3f939d74342ec4e
Trevor Blades <tdblades@gmail.com>
2019-02-24 10:49:59 -0800

    Remove spaces in RankingFilter keys

--- src/enums/RankingFilter.ts
+++ src/enums/RankingFilter.ts
@@ -1,7 +1,7 @@
 export const enum RankingFilter {
-  'Top 5' = 'Top5',
-  'Top 10' = 'Top10',
-  'Top 20' = 'Top20',
-  'Top 30' = 'Top30',
-  'Top 50' = 'Top50'
+  Top5 = 'Top5',
+  Top10 = 'Top10',
+  Top20 = 'Top20',
+  Top30 = 'Top30',
+  Top50 = 'Top50'
 }
