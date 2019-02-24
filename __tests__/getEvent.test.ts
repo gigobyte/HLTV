39ff32921cad7e89a6431a2954fe8065c21dd67e
gigobyte <gigobest2@gmail.com>
2019-02-24 21:12:41 +0200

    Update getEvent tests

--- __tests__/getEvent.test.ts
+++ __tests__/getEvent.test.ts
@@ -1,11 +1,15 @@
 import HLTV from '../src/'
 
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 test('getEvent', async () => {
   expect(await HLTV.getEvent({ id: 3452 })).toMatchSnapshot()
   expect(await HLTV.getEvent({ id: 3552 })).toMatchSnapshot()
   expect(await HLTV.getEvent({ id: 355 })).toMatchSnapshot()
+  await sleep(3000) // Sleep to prevent accidental ban by hltv
   expect(await HLTV.getEvent({ id: 3005 })).toMatchSnapshot()
   expect(await HLTV.getEvent({ id: 4356 })).toMatchSnapshot()
-  expect(await HLTV.getEvent({ id: 4409 })).toMatchSnapshot()
   expect(await HLTV.getEvent({ id: 2870 })).toMatchSnapshot()
 }, 30000)
