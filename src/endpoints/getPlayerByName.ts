924ae92d497b0ff60029d17b74a663c7ac3de8bc
Samuel Papke <samuel.papke@protonmail.com>
2019-10-08 13:11:13 +0200

    Removing useless errorhandling

--- src/endpoints/getPlayerByName.ts
+++ src/endpoints/getPlayerByName.ts
@@ -7,11 +7,10 @@ export const getPlayerByName = (config: HLTVConfig) => async ({
 }: {
   name: string
 }): Promise<FullPlayer> => {
-  if (config.loadPage === undefined) throw new Error('\'config.loadPage\' function is not Defined')
   const pageContent = JSON.parse(await config.loadPage(`${config.hltvUrl}/search?term=${name}`))
   let playerArr = pageContent[0].players[0]
 
-  let retArr = await HLTV.getPlayer({id: playerArr.id})
+  let retArr = await HLTV.getPlayer({ id: playerArr.id })
 
   return retArr
 }
