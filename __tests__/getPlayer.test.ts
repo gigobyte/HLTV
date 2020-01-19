01513327fc94957c8929b9ec241d281441a5054c
Stanislav Iliev <gigobest2@gmail.com>
2020-01-19 16:13:52 +0200

    Add tests

---
+++ __tests__/getPlayer.test.ts
@@ -0,0 +1,13 @@
+import HLTV from '../src/'
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+test('getPlayer', async () => {
+  expect(await HLTV.getPlayer({ id: 8000 })).toMatchSnapshot()
+  expect(await HLTV.getPlayer({ id: 7000 })).toMatchSnapshot()
+  await sleep(3000)
+  expect(await HLTV.getPlayer({ id: 3000 })).toMatchSnapshot()
+  expect(await HLTV.getPlayer({ id: 9123 })).toMatchSnapshot()
+}, 30000)
