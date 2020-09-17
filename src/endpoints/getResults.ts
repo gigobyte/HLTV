e813d212f64dfd9d6915d84dbc3b1517a6833060
Marc Helpenstein <helpi9007@gmail.com>
2020-09-17 08:55:22 +0200

    Add logoLink for getResults (#336)
    * Add logoLink for results
    
    * Edit code for pull request
    
    * Update snap

--- src/endpoints/getResults.ts
+++ src/endpoints/getResults.ts
@@ -1,6 +1,6 @@
 import { MatchResult } from '../models/MatchResult'
 import { Event } from '../models/Event'
-import { Team } from '../models/Team'
+import { ResultTeam } from '../models/ResultTeam'
 import { MapSlug } from '../enums/MapSlug'
 import { popSlashSource } from '../utils/parsing'
 import { HLTVConfig } from '../config'
@@ -54,18 +54,14 @@ export const getResults = (config: HLTVConfig) => async ({
         const id = Number(matchEl.attr('href')!.split('/')[2])
         const stars = matchEl.find('.stars i').length
 
-        const team1: Team = {
-          id:
-            Number(popSlashSource(matchEl.find('img.team-logo').first())) ||
-            undefined,
-          name: matchEl.find('div.team').first().text()
+        const team1: ResultTeam = {
+          name: matchEl.find('div.team').first().text(),
+          logo: matchEl.find('img.team-logo').first().attr('src')!
         }
 
-        const team2: Team = {
-          id:
-            Number(popSlashSource(matchEl.find('img.team-logo').last())) ||
-            undefined,
-          name: matchEl.find('div.team').last().text()
+        const team2: ResultTeam = {
+          name: matchEl.find('div.team').last().text(),
+          logo: matchEl.find('img.team-logo').last().attr('src')!
         }
 
         const result = matchEl.find('.result-score').text()
