44b72a1ced8d3a20c00fd2130fbfef83d646d1a8
Helpi90 <helpi9007@gmail.com>
2020-10-07 10:51:23 +0200

    Add logo link and date

--- src/endpoints/getOngoingEvents.ts
+++ src/endpoints/getOngoingEvents.ts
@@ -9,10 +9,14 @@ export const getOngoingEvents = (config: HLTVConfig) => async (): Promise<Ongoin
   toArray($('.tab-content'))
     .map((eventEl) => {
       toArray(eventEl.find('a')).map((a) => {
+        let dateSelector = '.eventDetails .col-desc span[data-unix]'
         const event = {
           name: a.attr('href')!.split('/')[3],
           id: Number(a.attr('href')!.split('/')[2]),
-          link: config.hltvUrl + a.attr('href')!
+          link: config.hltvUrl + a.attr('href')!,
+          logo: a.find('.logo').attr('src')!,
+          dateStart: a.find(dateSelector).eq(0).data('unix'),
+          dateEnd: a.find(dateSelector).eq(1).data('unix')
         }
         if (!ongoingEvents.includes(event))
           ongoingEvents.push(event);
--- src/models/OngoingEventResult.ts
+++ src/models/OngoingEventResult.ts
@@ -3,4 +3,7 @@ export interface OngoingEventResult {
   readonly name: string
   readonly id: number
   readonly link: string
+  readonly dateStart?: number
+  readonly dateEnd?: number
+  readonly logo: string
 }
