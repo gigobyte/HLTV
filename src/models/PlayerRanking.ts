1f2d284375c7c565f773db5e4aa334c0e751a809
ncla <ncla@users.noreply.github.com>
2021-01-15 22:24:55 +0200

    Feedback changes

--- src/models/PlayerRanking.ts
+++ src/models/PlayerRanking.ts
@@ -3,10 +3,10 @@ import { Team } from './Team'
 export interface PlayerRanking {
   readonly id?: number
   readonly name: string
-  readonly country?: string
-  readonly teams?: Team[]
-  readonly maps?: number
-  readonly kdDiff?: number
-  readonly kd?: number
-  readonly rating?: number
+  readonly country: string
+  readonly teams: Team[]
+  readonly maps: number
+  readonly kdDiff: number
+  readonly kd: number
+  readonly rating: number
 }
