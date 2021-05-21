8b15eef22fc8fb3a41a1512b1f198198d10fed33
Danilo Campana Fuchs <danilo.fuchs@ebanx.com>
2021-05-21 02:25:44 +0000

    chore: Rewrite defaultLoadPage behavior

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { Agent as HttpsAgent } from 'https'
 import { Agent as HttpAgent } from 'http'
-import * as request from 'request'
+import fetch from 'node-fetch'
 import randomUseragent from 'random-useragent'
 
 export interface HLTVConfig {
@@ -8,34 +8,27 @@ export interface HLTVConfig {
   httpAgent: HttpsAgent | HttpAgent
 }
 
-export const defaultLoadPage = (
-  httpAgent: HttpsAgent | HttpAgent | undefined
-) => (url: string) =>
-  new Promise<string>((resolve) => {
-    request.get(
-      url,
-      {
-        gzip: true,
-        agent: httpAgent,
-        headers: {
-          'User-Agent': randomUseragent.getRandom((ue) =>
-            [
-              '/Browsers - Windows',
-              '/Browsers - Linux',
-              '/Browsers - Mac'
-            ].includes(ue.folder)
-          )
-        }
-      },
-      (err, __, body) => {
-        if (err) {
-          throw err
-        }
-
-        resolve(body)
-      }
+export const defaultLoadPage =
+  (httpAgent: HttpsAgent | HttpAgent | undefined) => async (url: string) => {
+    const randomUe = randomUseragent.getRandom((ue) =>
+      ['/Browsers - Windows', '/Browsers - Linux', '/Browsers - Mac'].includes(
+        ue.folder
+      )
     )
-  })
+
+    let headers: HeadersInit = {}
+
+    if (randomUe) {
+      headers['user-agent'] = randomUe
+    }
+
+    const response = await fetch(url, {
+      agent: httpAgent,
+      headers
+    })
+
+    return await response.text()
+  }
 
 const defaultAgent = new HttpsAgent()
 
