300235acdb668ba2f6220c98b15a388c8686d779
gigobyte <gigobest2@gmail.com>
2021-08-06 19:56:45 +0300

    Add `rounds` in getPlayerRankings

--- src/endpoints/getPlayerRanking.ts
+++ src/endpoints/getPlayerRanking.ts
@@ -14,6 +14,7 @@ export interface PlayerRanking {
   teams: Team[]
   maps: number
   kdDiff: number
+  rounds: number
   kd: number
   rating1?: number
   rating2?: number
@@ -30,56 +31,60 @@ export interface GetPlayerRankingOptions {
   bestOfX?: BestOfFilter
 }
 
-export const getPlayerRanking = (config: HLTVConfig) => async (
-  options: GetPlayerRankingOptions = {}
-): Promise<PlayerRanking[]> => {
-  const query = stringify({
-    ...(options.startDate ? { startDate: options.startDate } : {}),
-    ...(options.endDate ? { endDate: options.endDate } : {}),
-    ...(options.matchType ? { matchType: options.matchType } : {}),
-    ...(options.rankingFilter ? { rankingFilter: options.rankingFilter } : {}),
-    ...(options.maps ? { maps: options.maps.map(toMapFilter) } : {}),
-    ...(options.minMapCount ? { minMapCount: options.minMapCount } : {}),
-    ...(options.countries ? { country: options.countries } : {}),
-    ...(options.bestOfX ? { bestOfX: options.bestOfX } : {})
-  })
-
-  const $ = HLTVScraper(
-    await fetchPage(
-      `https://www.hltv.org/stats/players?${query}`,
-      config.loadPage
-    )
-  )
-
-  return $('.player-ratings-table tbody tr')
-    .toArray()
-    .map((el) => {
-      const id = el.find('.playerCol a').attrThen('href', getIdAt(3))
-      const name = el.find('.playerCol a').text()
-      const player = { name, id }
-
-      const teams = el
-        .find('.teamCol a')
-        .toArray()
-        .map((teamEl) => ({
-          id: teamEl.attrThen('href', getIdAt(3)),
-          name: teamEl.find('img').attr('title')
-        }))
-
-      const maps = el.find('td.statsDetail').eq(0).numFromText()!
-      const kdDiff = el.find('td.kdDiffCol').numFromText()!
-      const kd = el.find('td.statsDetail').eq(1).numFromText()!
-      const rating = el.find('td.ratingCol').numFromText()!
-
-      return {
-        player,
-        teams,
-        maps,
-        kdDiff,
-        kd,
-        ...($('.ratingCol .ratingDesc').text() === '2.0'
-          ? { rating2: rating }
-          : { rating1: rating })
-      }
+export const getPlayerRanking =
+  (config: HLTVConfig) =>
+  async (options: GetPlayerRankingOptions = {}): Promise<PlayerRanking[]> => {
+    const query = stringify({
+      ...(options.startDate ? { startDate: options.startDate } : {}),
+      ...(options.endDate ? { endDate: options.endDate } : {}),
+      ...(options.matchType ? { matchType: options.matchType } : {}),
+      ...(options.rankingFilter
+        ? { rankingFilter: options.rankingFilter }
+        : {}),
+      ...(options.maps ? { maps: options.maps.map(toMapFilter) } : {}),
+      ...(options.minMapCount ? { minMapCount: options.minMapCount } : {}),
+      ...(options.countries ? { country: options.countries } : {}),
+      ...(options.bestOfX ? { bestOfX: options.bestOfX } : {})
     })
-}
+
+    const $ = HLTVScraper(
+      await fetchPage(
+        `https://www.hltv.org/stats/players?${query}`,
+        config.loadPage
+      )
+    )
+
+    return $('.player-ratings-table tbody tr')
+      .toArray()
+      .map((el) => {
+        const id = el.find('.playerCol a').attrThen('href', getIdAt(3))
+        const name = el.find('.playerCol a').text()
+        const player = { name, id }
+
+        const teams = el
+          .find('.teamCol a')
+          .toArray()
+          .map((teamEl) => ({
+            id: teamEl.attrThen('href', getIdAt(3)),
+            name: teamEl.find('img').attr('title')
+          }))
+
+        const maps = el.find('td.statsDetail').eq(0).numFromText()!
+        const rounds = el.find('td.statsDetail').eq(1).numFromText()!
+        const kdDiff = el.find('td.kdDiffCol').numFromText()!
+        const kd = el.find('td.statsDetail').eq(2).numFromText()!
+        const rating = el.find('td.ratingCol').numFromText()!
+
+        return {
+          player,
+          teams,
+          maps,
+          kdDiff,
+          rounds,
+          kd,
+          ...($('.ratingCol .ratingDesc').text() === '2.0'
+            ? { rating2: rating }
+            : { rating1: rating })
+        }
+      })
+  }
