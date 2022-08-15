30c649b55dd1357e8d3beacea583fa2ae632f108
Nils Jacobs <niljaco.pi@gmail.com>
2022-08-15 21:16:25 +0200

    fixed getTeamRAking

--- src/endpoints/getTeamRanking.ts
+++ src/endpoints/getTeamRanking.ts
@@ -74,7 +74,7 @@ export const getTeamRanking =
         }
 
         const changeText = el.find('.change').text()
-        const isNew = changeText === 'New'
+        const isNew = changeText === 'NEW TEAM'
         const change = changeText === '-' || isNew ? 0 : Number(changeText)
 
         return { points, place, team, change, isNew }
