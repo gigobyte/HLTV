fd985bfd99d112bbacc3b34d1152040082c79db1
gigobyte <gigobest2@gmail.com>
2022-12-02 19:19:36 +0200

    Fix getEventByName returns a JSON error of no event is found #617

--- src/endpoints/getEventByName.ts
+++ src/endpoints/getEventByName.ts
@@ -4,14 +4,18 @@ import { FullEvent, getEvent } from './getEvent'
 export const getEventByName =
   (config: HLTVConfig) =>
   async ({ name }: { name: string }): Promise<FullEvent> => {
-    const pageContent = JSON.parse(
-      await config.loadPage!(`https://www.hltv.org/search?term=${name}`)
-    )
-    const firstResult = pageContent[0].events[0]
+    try {
+      const pageContent = JSON.parse(
+        await config.loadPage!(`https://www.hltv.org/search?term=${name}`)
+      )
+      const firstResult = pageContent[0].events[0]
 
-    if (!firstResult) {
+      if (!firstResult) {
+        throw new Error(`Event ${name} not found`)
+      }
+
+      return getEvent(config)({ id: firstResult.id })
+    } catch {
       throw new Error(`Event ${name} not found`)
     }
-
-    return getEvent(config)({ id: firstResult.id })
   }
