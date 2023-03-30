684d8785f68606e8ee4f88f38dbcd3ce75daf7e6
gigobyte <gigobest2@gmail.com>
2023-03-30 14:20:29 +0300

    Use got-scraping as default request library

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import { Agent as HttpsAgent } from 'https'
 import { Agent as HttpAgent } from 'http'
-import * as request from 'request'
+import { gotScraping } from 'got-scraping'
 
 export interface HLTVConfig {
   loadPage: (url: string) => Promise<string>
@@ -9,22 +9,9 @@ export interface HLTVConfig {
 
 export const defaultLoadPage =
   (httpAgent: HttpsAgent | HttpAgent | undefined) => (url: string) =>
-    new Promise<string>((resolve) => {
-      request.get(
-        url,
-        {
-          gzip: true,
-          agent: httpAgent
-        },
-        (err, __, body) => {
-          if (err) {
-            throw err
-          }
-
-          resolve(body)
-        }
-      )
-    })
+    gotScraping({ url, agent: { http: httpAgent, https: httpAgent } }).then(
+      (res) => res.body
+    )
 
 const defaultAgent = new HttpsAgent()
 
